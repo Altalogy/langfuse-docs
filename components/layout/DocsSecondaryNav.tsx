24f04da103d638d3f7b091bda5c6074f2418791a
Tom Antas <tom@altalogy.com>
2026-04-07 09:08:27 +0200

    remove MenuSwitcher

--- components/layout/DocsSecondaryNav.tsx
+++ components/layout/DocsSecondaryNav.tsx
@@ -33,7 +33,7 @@ export function DocsSecondaryNav() {
                 )}
               >
                 <item.Icon className="w-4 h-4 shrink-0" />
-                <span>{item.title}</span>
+                aa<span>{item.title}</span>
               </Link>
             );
           })}
